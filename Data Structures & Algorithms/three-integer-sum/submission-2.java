4438e1566675fa93e3454066adbf938c8c4b977e
Shireen Bano <Shireenbanu89@gmail.com>
2026-06-01 23:22:30 -0700

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> arrList = new ArrayList<>();
+        Set<String> hashSet = new HashSet<>();
+        Arrays.sort(nums);
+    //    System.out.println("arr: "+Arrays.toString(nums));
+       for( int i=0; i<nums.length; i++){
+        
+        int target = nums[i]*-1;
+        int low =i+1;
+        int high = nums.length -1;
+        // System.out.println("for item 1: "+ nums[i]);
+
+        while(low<high){
+            
+            int sum = (nums[low]+nums[high]);
+            if(sum == target && low!=i && high !=i){
+                // System.out.println("found ans: "+nums[low]+" "+nums[high] + " = "+ sum );
+                List<Integer> arr = new ArrayList<>(Arrays.asList(nums[i], nums[low], nums[high]));
+                Collections.sort(arr);
+                // System.out.println("arr: "+(arr.toString()));
+            
+                if(!hashSet.contains(arr.toString())){
+                    hashSet.add(arr.toString());
+                    arrList.add(arr);
+                }
+                low++;
+                high--;
+            }
+            else if(sum<target){
+              low++;
+            }
+            else{
+                high--;
+            }   
+        }
+        
+       }
+       return arrList;
+    }
+}
